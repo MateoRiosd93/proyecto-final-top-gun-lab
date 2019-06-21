c1c3e539c4488ecca1643b6a91e5e67b59e5059c
Santiago <santi_diaz17@hotmail.com>
2019-06-21 17:19:03 -0500

    Creación de los componentes, Rutas y desarrollo del componente Header (Responsive)

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
+import "../styles/Header.css";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/menu.svg";
 
-export default function header() {
-    return (
-        <div>
-            <h1>Hola mundo</h1>
-        </div>
-    )
-}
+const Header = () => {
+
+  return (
+    <nav className="navbar">
+      <div className="container_logo">
+        <NavLink to="/employees" className="nav_link">
+          <img
+            src="https://www.teaminternational.com/wp-content/uploads/2019/05/new-web-logo-team-international.png"
+            alt=""
+          />
+        </NavLink>
+      </div>
+
+      <div className="menu_responsive" /*onClick={(e) => handleChangeFlag(e)}*/ >
+        <img src={logo} alt="menu responsive" />
+      </div>
+ <ul className="nav_list">
+        <li className="nav_list-item">
+          <NavLink
+            to="/employees"
+            className="nav_link"
+            activeClassName="active"
+          >
+            Employees
+          </NavLink>
+        </li>
+        <li className="nav_list-item">
+          <NavLink to="/prizes" className="nav_link" activeClassName="active">
+            Prizes
+          </NavLink>
+        </li>
+        <li className="nav_list-item">
+          <NavLink
+            to="/achivements"
+            className="nav_link"
+            activeClassName="active"
+          >
+            Achivements
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
