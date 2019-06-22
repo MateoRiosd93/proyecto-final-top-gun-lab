6233e1b581bb66485212e4151909924a05d28823
Santiago <santi_diaz17@hotmail.com>
2019-06-22 12:58:51 -0500

    desarrollo del componente employee/id

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { NavLink } from "react-router-dom";
 import ShowElements from './ShowElements';
 import Search from './Search';
+import Employee from './Employee';
 import { BASE_LOCAL_ENDPOINT } from '../constants';
 import axios from 'axios';
 
@@ -38,12 +40,14 @@ class Employees extends Component {
                 <div className="container-elements">
                 {
                     employees.map(({id,name,imgSrc,points}) =>
+                    <NavLink key={id} to={`/employee/${id}`}>
                         <ShowElements
-                            key={id}
-                            name={name}
-                            imgSrc={imgSrc}
-                            points={points}/>
-                     )
+                                key={id}
+                                name={name}
+                                imgSrc={imgSrc}
+                                points={points}/>
+                    </NavLink>
+                    )
                 }
                 </div>
             </div>
