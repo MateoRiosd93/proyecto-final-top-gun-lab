b7ae6c58d7e4ebdfa60cbf6f58d7261ec223bf43
Santiago <santi_diaz17@hotmail.com>
2019-06-22 07:27:18 -0500

    módulo Prizes: Obtencion de la data

--- src/components/Prizes.js
+++ src/components/Prizes.js
@@ -1,12 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {BASE_LOCAL_ENDPOINT} from '../constants';
+import axios from 'axios';
 
 class Prizes extends Component {
 
     state = {
-        pirzes: []
+        pirzes: [],
+        error: false
     }
 
-    componentDidMount 
+
+    getPrizesDB = () => {
+        axios.get(`${BASE_LOCAL_ENDPOINT}/prizes`)
+        .then(response => {
+            console.log(response)
+            
+            // this.setState({
+            //     pirzes: response.data
+            // })
+        })
+        .catch(error => {
+            this.setState({
+                error: true
+            })
+        })
+    }
+
+
+    componentDidMount = () => {
+
+    }
 
 
     render() {
