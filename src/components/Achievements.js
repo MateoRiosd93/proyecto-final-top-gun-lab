e35d6279fc383278c33b544f29677532358f1910
Santiago <santi_diaz17@hotmail.com>
2019-06-29 12:38:54 -0500

    Funcionalidad Delete del Componente Achievements

--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -3,14 +3,19 @@ import { BASE_LOCAL_ENDPOINT } from "../constants";
 import Search from "./Search";
 import axios from "axios";
 import ShowAchievements from "./ShowAchievements";
-
+import ModalEditAchievement from "./ModalEditAchievement";
+import DeleteMessage from './DeleteMessage';
 import "../styles/Achievements.css";
 
 export default class Achievements extends Component {
   state = {
     achievements: [],
     isLoad: false,
-    searchName: ""
+    searchName: "",
+    showModal: false,
+    showDeleteMessage: false,
+    idAchievement: "",
+    error:""
   };
 
   getAchievementsBD = () => {
@@ -39,11 +44,79 @@ export default class Achievements extends Component {
     });
   };
 
+  handleShowModal = (id = null) => {
+    const idAchievement = id;
+
+    this.setState(prevState => ({
+      ...prevState,
+      showModal: !prevState.showModal,
+      idAchievement
+    }));
+  };
+
+  editAchievement = ( name, points ) => {
+    const id = this.state.idAchievement;
+
+    console.log('Componente Achievement');
+    console.log(`name ${name}, points ${points}, id ${id}`);
+
+    axios
+      .put(
+        `${BASE_LOCAL_ENDPOINT}achievements/${id}`,
+        {
+          name,
+          points
+        },
+        {
+          headers: { "Content-Type": "application/json" }
+        }
+      )
+      .then(() => {
+        this.getAchievementsBD();
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+
+    this.handleShowModal();
+  };
+
+  handleShowDeleteMessage = (id=null) => {
+    const idAchievement = id
+    this.setState(prevState => ({
+      ...prevState,
+      showDeleteMessage: !prevState.showDeleteMessage,
+      idAchievement
+    }))
+  }
+
+  deleteAchievement = () => {
+    const {idAchievement:id} = this.state;
+
+    axios.delete(`${BASE_LOCAL_ENDPOINT}achievements/${id}`)
+    .then(() => {
+      this.getAchievementsBD();
+      console.log('achievement eliminado');
+    })
+    .catch(err => {
+      this.setState({
+        error: err.message
+      });
+    });
+    this.handleShowDeleteMessage();
+  }
+
+  
+
   render() {
-    const { achievements, searchName } = this.state;
+    const { achievements, searchName, showModal, showDeleteMessage, idAchievement } = this.state;
     const achievementsFilter = achievements.filter(achievement =>
       achievement.name.toLowerCase().includes(searchName.toLowerCase())
     );
+
+    console.log(idAchievement);
 
     return (
       <div className="container-achievements">
@@ -53,9 +126,27 @@ export default class Achievements extends Component {
             mensaje="This is our Achievements!"
           />
         </div>
+        {showModal && (
+          <ModalEditAchievement
+            handleShowModal={this.handleShowModal}
+            editAchievement={this.editAchievement}
+          />
+        )}
+        {
+          showDeleteMessage && (<DeleteMessage 
+            handleShowDeleteMessage={this.handleShowDeleteMessage}
+            deleteAchievement={this.deleteAchievement}
+                                />)
+        }
         <ul className="list-achievements">
           {achievementsFilter.map(({ id, name, points }) => (
-            <ShowAchievements key={id} name={name} points={points} />
+            <ShowAchievements
+              key={id}
+              name={name}
+              points={points}
+              handleShowModal={() => this.handleShowModal(id)}
+              handleShowDeleteMessage={()=>this.handleShowDeleteMessage(id)}
+            />
           ))}
         </ul>
       </div>
--- src/components/ShowAchievements.js
+++ src/components/ShowAchievements.js
@@ -1,19 +1,24 @@
-import React from 'react';
-
+import React from 'react'
 import '../styles/ShowAchievements.css';
 
-export default function ShowAchievements({name,points}) {
-    return (
+
+const ShowAchievements = ({name,points,handleShowModal,handleShowDeleteMessage}) => {
+    return ( 
         <li className="container-achievement">
+            <div className="container-name-achievement">
+
             <h1
                 className="name-achievement">{name}
                     <span className="points-achievement"><span className="start icon-star-full"></span> {points}</span>
             </h1>
-            <div className="container-achievement-buttons">
-                <button className="button-achievement-edit">Edit</button>
-                <button className="button-achievement-delete">Delete</button>
             </div>
-
+            <div className="container-achievement-buttons">
+                <button className="button-achievement-edit" onClick={handleShowModal} >Edit</button>
+                <button className="button-achievement-delete" onClick={handleShowDeleteMessage} >Delete</button>
+            </div>
         </li>
-    )
+     );
 }
+ 
+export default ShowAchievements;
+
