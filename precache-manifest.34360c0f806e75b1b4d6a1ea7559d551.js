6b34e520fcd2ffae729f0b404e8bed8a2e83ec10
Mateo Rios Diaz <40178754+MateoRiosd93@users.noreply.github.com>
2019-06-30 22:03:07 -0500

    Updates

---
+++ precache-manifest.34360c0f806e75b1b4d6a1ea7559d551.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87873d2f7d29b50efeef253f8684499",
+    "url": "/proyecto-final-top-gun-lab/index.html"
+  },
+  {
+    "revision": "5daeb3cfad583b9e0977",
+    "url": "/proyecto-final-top-gun-lab/static/css/main.48eaa4b3.chunk.css"
+  },
+  {
+    "revision": "b2d01294214fa2d9f1f5",
+    "url": "/proyecto-final-top-gun-lab/static/js/2.def3d402.chunk.js"
+  },
+  {
+    "revision": "5daeb3cfad583b9e0977",
+    "url": "/proyecto-final-top-gun-lab/static/js/main.533b7ffe.chunk.js"
+  },
+  {
+    "revision": "acf511205ce4dad06616",
+    "url": "/proyecto-final-top-gun-lab/static/js/runtime~main.3403865f.js"
+  },
+  {
+    "revision": "5fa1541a3978264e287783c296d240cc",
+    "url": "/proyecto-final-top-gun-lab/static/media/TEAM.5fa1541a.PNG"
+  },
+  {
+    "revision": "9ad75627e0b126cc9fee98e50be009bb",
+    "url": "/proyecto-final-top-gun-lab/static/media/WeAreTEAM.9ad75627.PNG"
+  },
+  {
+    "revision": "09d1517a439152c4d0a2d558cea468c2",
+    "url": "/proyecto-final-top-gun-lab/static/media/icomoon.09d1517a.svg"
+  },
+  {
+    "revision": "3be76bd060773639ffd76668b01a878c",
+    "url": "/proyecto-final-top-gun-lab/static/media/icomoon.3be76bd0.woff"
+  },
+  {
+    "revision": "91cf2d39e8346e1e40d933edc01da7ca",
+    "url": "/proyecto-final-top-gun-lab/static/media/icomoon.91cf2d39.eot"
+  },
+  {
+    "revision": "9f03016f1a0e6cf2cf52f175193d5f6e",
+    "url": "/proyecto-final-top-gun-lab/static/media/icomoon.9f03016f.ttf"
+  }
+]);
